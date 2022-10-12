4c57bb6f818fefe83fc197f9d7d3e1f4a441a4f5
TygoHouweling <85113680+TygoHouweling@users.noreply.github.com>
2022-10-12 14:05:47 +0200

    added discount items

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,23 @@
 function calculator() {
     total = document.querySelector('#total_items').value;
 
+    let discound =0;
     if(total>=50000){
-
+        let discound = 15;
     } else {
         if(total>=10000){
-
+            let discound = 10;
         } else {
             if(total>=7000){
-
+                let discound = 7;
             } else {
                 if(total>=5000){
+                    let discound = 5;
 
                 } else {
                     if(total>=1000){
+                        let discound = 3;
 
-                    } else {
-                        
                     }
                 }
             }
