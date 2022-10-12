ec0b11c6b51d76837e27af5d5c3e4c4cd01fd216
TygoHouweling <85113680+TygoHouweling@users.noreply.github.com>
2022-10-12 13:59:23 +0200

    if statements

---
+++ javascript/javascript.js
@@ -0,0 +1,26 @@
+function calculator() {
+    total = document.querySelector('#price').value;
+
+    if(total>=50000){
+
+    } else {
+        if(total>=10000){
+
+        } else {
+            if(total>=7000){
+
+            } else {
+                if(total>=5000){
+
+                } else {
+                    if(total>=1000){
+
+                    } else {
+                        
+                    }
+                }
+            }
+        }
+    }
+    document.querySelector('.total').innerHTML = total;
+}
